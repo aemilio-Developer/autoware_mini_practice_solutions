8c595049fe2b71afad6d9d4c10eeacf73931c965
aemilio-Developer <e.amato16@studenti.unisa.it>
2025-07-28 13:47:05 +0300

    updating launch file for node reusability, adding rate parameter to publisher node

--- nodes/publisher.py
+++ nodes/publisher.py
@@ -3,9 +3,11 @@ import rospy
 from std_msgs.msg import String
 
 rospy.init_node('publisher')
-rate = rospy.Rate(2)
+hz_rate = message = rospy.get_param('~publish_frequency', 10)
+rate = rospy.Rate(hz_rate)
 pub = rospy.Publisher('/message', String, queue_size=10)
+message = rospy.get_param('~message', 'Hello World!')
 
 while not rospy.is_shutdown():
-    pub.publish("Hello world!")
+    pub.publish(message)
     rate.sleep()
